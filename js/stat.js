4ec4d03163eb7e31a1157118d126f481960c6b02
AVSStorage <33234008+AVSStorage@users.noreply.github.com>
2018-04-05 23:48:25 +0300

    "Create a result function"

--- js/stat.js
+++ js/stat.js
@@ -26,41 +26,41 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
-function createRect(ctx, rectX, rectY, rectHeight) {
-  ctx.fillRect(rectX, rectY, BAR_WIDTH, rectHeight);
+function generateColor(ctx, color) {
+  ctx.fillStyle = color;
 }
 
-function generateColor(name) {
-  if (name === 'Вы') {
-    return 'rgba(255, 0, 0 , 1)';
-  } else {
-    return 'hsl(240,' + Math.floor(Math.random() * 100) + '%,50%)';
-  }
+function createResults(ctx, maxTime, times, name, rectX, textColor, barColor) {
+  var rectHeight = (BAR_HEIGHT * times) / maxTime;
+  var rectY = CLOUD_Y + BAR_HEIGHT - rectHeight + BAR_GAP * 2 - GAP;
+  var textY = CLOUD_HEIGHT - rectHeight - FONT_GAP * 2 - GAP;
+  ctx.fillStyle = barColor;
+  ctx.fillRect(rectX, rectY, BAR_WIDTH, rectHeight);
+  ctx.fillStyle = textColor;
+  ctx.fillText(name, rectX, CLOUD_HEIGHT - FONT_GAP);
+  ctx.fillText(Math.round(times), rectX, textY);
 }
 
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
-  var maxTime = getMaxElement(times);
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
   ctx.fillText('Ура вы победили!', CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP);
   ctx.fillText('Список результатов:', CLOUD_X + GAP, CLOUD_Y + GAP * 2 + FONT_GAP * 2);
-
-  ctx.fillStyle = '#000';
+  var maxTime = getMaxElement(times);
   var rectX;
-  var rectY;
-  var rectHeight;
+  var textColor = '#000';
+  var barColor;
   for (var i = 0; i < names.length; i++) {
     rectX = CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i;
-    rectHeight = (BAR_HEIGHT * times[i]) / maxTime;
-    rectY = CLOUD_Y + BAR_HEIGHT - rectHeight + BAR_GAP * 2 - GAP;
-    ctx.fillStyle = generateColor(names[i]);
-    createRect(ctx, rectX, rectY, rectHeight);
-    ctx.fillStyle = '#000';
-    ctx.fillText(names[i], rectX, CLOUD_HEIGHT - FONT_GAP);
-    ctx.fillText(Math.round(times[i]), rectX, CLOUD_HEIGHT - rectHeight - FONT_GAP * 2 - GAP);
+    if (names[i] === 'Вы') {
+      barColor = generateColor(ctx, 'rgba(255, 0, 0 , 1)');
+    } else {
+      barColor = generateColor(ctx, 'hsl(240,' + Math.floor(Math.random() * 100) + '%,50%)');
+    }
+    createResults(ctx, maxTime, times[i], names[i], rectX, textColor, barColor);
   }
 };
 
